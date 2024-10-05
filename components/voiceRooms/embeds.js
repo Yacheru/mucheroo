707fbc2bb4d9644f0fbb1b231cc4c27ba4b88e28
Yacheru <amilohin274@gmail.com>
2024-10-06 00:28:31 +1100

    remove timestamps from voice-rooms embeds

--- components/voiceRooms/embeds.js
+++ components/voiceRooms/embeds.js
@@ -5,68 +5,71 @@ const danger = Colors.Red;
 const warn = Colors.Yellow;
 const success = Colors.Green;
 
-const templateEmbed = new EmbedBuilder()
-    .setImage(config.images.transperentImage)
-    .setTimestamp();
-
-
 module.exports = {
     notInVoice: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(warn)
-            .setDescription(`- Для взаимодействия создайте комнату - ${channelMention(config.channels.newChannelCreater)}`);
+            .setDescription(`- Для взаимодействия создайте комнату - ${channelMention(config.channels.newChannelCreater)}`)
+            .setImage(config.images.transperentImage);
     },
     notOwner: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- Вы не являетесь создателем комнаты!');
+            .setDescription('- Вы не являетесь создателем комнаты!')
+            .setImage(config.images.transperentImage);
     },
     notCorrectUser: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- Некорректный пользователь.');
+            .setDescription('- Некорректный пользователь.')
+            .setImage(config.images.transperentImage);
     },
     lockOrOpenRoom: function(message, memberId) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- Вы успешно ${message} комнату пользователю ${userMention(memberId)}`);
+            .setDescription(`- Вы успешно ${message} комнату пользователю ${userMention(memberId)}`)
+            .setImage(config.images.transperentImage);
     },
     transferRoom: function(memberId) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- Вы успешно передали владение комнатой пользователю ${userMention(memberId)}`);
+            .setDescription(`- Вы успешно передали владение комнатой пользователю ${userMention(memberId)}`)
+            .setImage(config.images.transperentImage);
     },
     noTemplate: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- У вас нет созданного шаблона!');
+            .setDescription('- У вас нет созданного шаблона!')
+            .setImage(config.images.transperentImage);
     },
     createTemplateSuccess: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription('- Ваш шаблон успешно создан!');
+            .setDescription('- Ваш шаблон успешно создан!')
+            .setImage(config.images.transperentImage);
     },
     deleteTemplateSuccess: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription('- Ваш шаблон успешно удалён!');
+            .setDescription('- Ваш шаблон успешно удалён!')
+            .setImage(config.images.transperentImage);
     },
     cancelCreateTemplate: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription('- Вы успешно отменили запрос на создание комнаты!');
+            .setDescription('- Вы успешно отменили запрос на создание комнаты!')
+            .setImage(config.images.transperentImage);
     },
     templateChannel: function(move) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- Комната изменена на **${move}**!`);
+            .setDescription(`- Комната изменена на **${move}**!`)
+            .setImage(config.images.transperentImage);
     },
     haveOrCreateTemplate: function(member, templateRow, voiceChannel) {
-        const haveOrCreateEmbed = new EmbedBuilder(templateEmbed)
+        const haveOrCreateEmbed = new EmbedBuilder()
             .setAuthor({ name: member.displayName, iconURL: member.displayAvatarURL() })
             .setThumbnail(member.displayAvatarURL());
-
-
         if (templateRow) {
             return haveOrCreateEmbed
                 .setColor(danger)
@@ -74,7 +77,8 @@ module.exports = {
                 .addFields(
                     { name: 'Настройки', value: `- Название: **${templateRow.channelName}**\n- Лимит участников: **${templateRow.channelLimit === 0 ? 'Без ограничений' : templateRow.channelLimit}**\n- Битрейт: **${templateRow.channelBitrate}**\n- Дата создания: ${time(new Date(templateRow.createdAt), 'R')}` },
                 )
-                .setFooter({ text: 'Нажмите на кнопку ниже, чтобы удалить шаблон' });
+                .setFooter({ text: 'Нажмите на кнопку ниже, чтобы удалить шаблон' })
+                .setImage(config.images.transperentImage);
         }
         else {
             return haveOrCreateEmbed
@@ -83,57 +87,64 @@ module.exports = {
                 .addFields(
                     { name: 'Настройки:', value: `- Название: **${voiceChannel.name}**\n- Лимит участников: **${voiceChannel.userLimit === 0 ? 'Без ограничений' : voiceChannel.userLimit}**\n- Битрейт: **${voiceChannel.bitrate}**` },
                 )
-                .setFooter({ text: 'Вы можете предложить другие параметры для хранения в шаблоне - @yacheru' });
+                .setFooter({ text: 'Вы можете предложить другие параметры для хранения в шаблоне - @yacheru' })
+                .setImage(config.images.transperentImage);
         }
     },
     newName: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- ${config.tmpvoiceIcons.name} Название канала успешно изменено!`);
+            .setDescription(`- ${config.tmpvoiceIcons.name} Название канала успешно изменено!`)
+            .setImage(config.images.transperentImage);
     },
     newLimit: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- ${config.tmpvoiceIcons.limit} Лимит участников успешно изменен!`);
+            .setDescription(`- ${config.tmpvoiceIcons.limit} Лимит участников успешно изменен!`)
+            .setImage(config.images.transperentImage);
     },
     maxLimit: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription(`- ${config.tmpvoiceIcons.upslot} Лимит участников достиг максимума!`);
+            .setDescription(`- ${config.tmpvoiceIcons.upslot} Лимит участников достиг максимума!`)
+            .setImage(config.images.transperentImage);
     },
     minLimit: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription(`- ${config.tmpvoiceIcons.downslot} Лимит участников достиг минимума!`);
+            .setDescription(`- ${config.tmpvoiceIcons.downslot} Лимит участников достиг минимума!`)
+            .setImage(config.images.transperentImage);
     },
     hideChannel: function(message) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- ${config.tmpvoiceIcons.hide} Комната успешно ${message}!`);
+            .setDescription(`- ${config.tmpvoiceIcons.hide} Комната успешно ${message}!`)
+            .setImage(config.images.transperentImage);
     },
     privateChannel: function(message) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- ${config.tmpvoiceIcons.private} Комната успешно ${message}!`);
+            .setDescription(`- ${config.tmpvoiceIcons.private} Комната успешно ${message}!`)
+            .setImage(config.images.transperentImage);
     },
     noOpenAdminRoom: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- Комнату администратора нельзя открыть публично! Перекиньте пользователя из другого канала или используйте команду </room access:1202279762689806416>');
+            .setDescription('- Комнату администратора нельзя открыть публично! Перекиньте пользователя из другого канала или используйте команду </room access:1202279762689806416>')
+            .setImage(config.images.transperentImage);
     },
     infoChannel: function(channel, memberAvatar, ownerId, adminRoom, templateRoom, members) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
             .setTitle(`Комната - ${channel.name}`)
             .setThumbnail(memberAvatar)
             .setFields(
                 { name: 'Настройки', value: `- Название: ${channel.name}\n- Лимит участников: ${channel.members.size}/${channel.userLimit}\n- Битрейт: ${channel.bitrate}\n- Создана: ${time(channel.createdAt, 'R')}\n- Создатель: ${userMention(ownerId)}\n- Комната администраторов: ${adminRoom}\n- Шаблонная: ${templateRoom}` },
                 { name: 'Участники', value: `${members}` },
-            )
-            .setTimestamp();
+            );
     },
     tempRoomsInfo: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
             .setFields(
                 { name: 'Про кнопки:', value: `- ${config.tmpvoiceIcons.upslot} / ${config.tmpvoiceIcons.downslot} - Данные кнопки изменяют лимит вашей комнаты на 1 в + или -.\n- ${config.tmpvoiceIcons.hide} - Данная кнопка скрывает/показывает ваш канал в списке каналов.\n- ${config.tmpvoiceIcons.private} - Данная кнопка открывает/скрывает ваш канал.\n- ${config.tmpvoiceIcons.voice} - Включить или выключить пользователю в вашей комнате микрофон.\n- ${config.tmpvoiceIcons.template} - Создать свой шаблон. Потом его можно применить в меню выбора.\n- ${config.tmpvoiceIcons.limit} - Поможет вам изменить лимит участников комнаты до желаемого.\n- ${config.tmpvoiceIcons.name} - Используйте, если нужно изменить название своей комнаты.\n- ${config.tmpvoiceIcons.info} - Узнать информацию о канале в котором вы находитесь.\n- ${config.tmpvoiceIcons.more} - Вызовет данное осведомительное сообщение вновь.`, inline: false },
@@ -143,38 +154,45 @@ module.exports = {
             );
     },
     changeBitrateSuccess: function(bitrate, userBitrate) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- Успешно установлено ${bitrate} (${userBitrate})`);
+            .setDescription(`- Успешно установлено ${bitrate} (${userBitrate})`)
+            .setImage(config.images.transperentImage);
     },
     notSupportedBitrate: function(boostLevel) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription(`- Сервер не поддерживает данный уровень битрейта! Необходим - **${boostLevel}** уровень буста.`);
+            .setDescription(`- Сервер не поддерживает данный уровень битрейта! Необходим - **${boostLevel}** уровень буста.`)
+            .setImage(config.images.transperentImage);
     },
     noUsersInVoice: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- Это действие невозможно, потому что в комнате нет никого, кроме вас.');
+            .setDescription('- Это действие невозможно, потому что в комнате нет никого, кроме вас. :(')
+            .setImage(config.images.transperentImage);
     },
     searchUserFail: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- Не удалось найти пользователя!');
+            .setDescription('- Не удалось найти пользователя!')
+            .setImage(config.images.transperentImage);
     },
     userNotInVoice: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- Пользователь не находится в голосовом канале!');
+            .setDescription('- Пользователь не находится в голосовом канале!')
+            .setImage(config.images.transperentImage);
     },
     muteOrUnmuteUser: function(message, memberId) {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(success)
-            .setDescription(`- Вы успешно ${message}и пользователя ${userMention(memberId)}`);
+            .setDescription(`- Вы успешно ${message}и пользователя ${userMention(memberId)}`)
+            .setImage(config.images.transperentImage);
     },
     notCorrectNumber: function() {
-        return new EmbedBuilder(templateEmbed)
+        return new EmbedBuilder()
             .setColor(danger)
-            .setDescription('- Введите целочисленное значение от 0 до 99!');
+            .setDescription('- Введите целочисленное значение от 0 до 99!')
+            .setImage(config.images.transperentImage);
     },
 };
