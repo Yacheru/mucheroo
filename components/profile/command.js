93e5ba55801f23d2b5bdb5b72a8ebde8ac19eb65
Yacheru <amilohin274@gmail.com>
2024-11-26 14:55:59 +1100

    fix

--- components/profile/command.js
+++ components/profile/command.js
@@ -1,19 +1,19 @@
-const { connections } = require('../../database/models/webApplication');
-
-const buttonCollector = require('./buttons');
-const embedCollector = require('./embed');
-
-
-module.exports = {
-    profileCommand: async function(interaction) {
-        await interaction.deferReply({ ephemeral: true });
-        const member = interaction.options.getMember('member') ?? interaction.member;
-
-        const connectionsRow = await connections.findByPk(member.id);
-
-        const buttonsComponents = await buttonCollector.buttonsCollector(connectionsRow);
-        const embedComponents = await embedCollector.profileEmbed(member, connectionsRow);
-
-        return await interaction.editReply({ content: '', embeds: [embedComponents], components: [buttonsComponents], ephemeral: true });
-    },
-};
+// const { connections } = require('../../database/models/webApplication');
+//
+// const buttonCollector = require('./buttons');
+// const embedCollector = require('./embed');
+//
+//
+// module.exports = {
+//     profileCommand: async function(interaction) {
+//         await interaction.deferReply({ ephemeral: true });
+//         const member = interaction.options.getMember('member') ?? interaction.member;
+//
+//         const connectionsRow = await connections.findByPk(member.id);
+//
+//         const buttonsComponents = await buttonCollector.buttonsCollector(connectionsRow);
+//         const embedComponents = await embedCollector.profileEmbed(member, connectionsRow);
+//
+//         return await interaction.editReply({ content: '', embeds: [embedComponents], components: [buttonsComponents], ephemeral: true });
+//     },
+// };
